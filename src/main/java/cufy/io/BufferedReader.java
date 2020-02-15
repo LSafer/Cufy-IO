e8598c716d6a08aa197c1c1b5b68c90a4271935d
LSafer <lsaferse@gmail.com>
2020-02-15 14:30:32 +0300

    Created Buffer, BufferedInputStream, BufferedReader
    As not all readers, and input-streams do not support mark(), So one of the solutions is to store data temporary using buffers.

---
+++ src/main/java/cufy/io/BufferedReader.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright (c) 2019, LSafer, All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * -You can edit this file (except the header).
+ * -If you have change anything in this file. You
+ *   shall mention that this file has been edited.
+ *   By adding a new header (at the bottom of this header)
+ *   with the word "Editor" on top of it.
+ */
+package cufy.io;
+
+import cufy.nio.Buffer;
+import cufy.util.Reflect$;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+import java.util.Objects;
+
+/**
+ * A boxing for readers as a workaround to support the methods {@link #mark} and {@link #reset()}. Using a {@link Buffer}.
+ *
+ * @author LSaferSE
+ * @version 2 release (14-Feb-2020)
+ * @since 10-Jan-2020
+ */
+public class BufferedReader extends Reader {
+	/**
+	 * The buffer that stores the marked characters.
+	 */
+	protected Buffer<Integer> buffer;
+	/**
+	 * The reader that this delegate is delegating to.
+	 */
+	protected Reader reader;
+
+	/**
+	 * Construct a new delegate that delegates to the given reader.
+	 *
+	 * @param reader to delegate to
+	 * @throws NullPointerException if the given reader is null
+	 */
+	public BufferedReader(Reader reader) {
+		super(Objects.requireNonNull(reader, "reader"));
+		this.reader = reader;
+	}
+
+	/**
+	 * Construct a new delegate that delegates to the given reader.
+	 *
+	 * @param reader to delegate to
+	 * @param lock   the lock to synchronize operations to
+	 * @throws NullPointerException if any of the given parameters is null
+	 */
+	public BufferedReader(Reader reader, Object lock) {
+		super(lock);
+		this.reader = reader;
+	}
+
+	@Override
+	public int read(CharBuffer target) throws IOException {
+		synchronized (this.lock) {
+			Objects.requireNonNull(target, "target");
+
+			int len = target.remaining();
+			char[] cbuf = new char[len];
+
+			int n = this.read(cbuf, 0, len);
+
+			if (n > 0)
+				target.put(cbuf, 0, n);
+
+			return n;
+		}
+	}
+	@Override
+	public int read() throws IOException {
+		synchronized (super.lock) {
+			this.ensureOpen();
+			if (this.buffer != null && this.buffer.hasNext()) {
+				return (int) Reflect$.primitiveCast(int.class, this.buffer.read());
+			} else {
+				int value = this.reader.read();
+
+				if (value != -1 && this.buffer != null)
+					this.buffer.write(value);
+
+				return value;
+			}
+		}
+	}
+	@Override
+	public int read(char[] cbuf) throws IOException {
+		synchronized (this.lock) {
+			Objects.requireNonNull(cbuf, "cbuf");
+			return this.read(cbuf, 0, cbuf.length);
+		}
+	}
+	@Override
+	public int read(char[] cbuf, int off, int len) throws IOException {
+		synchronized (super.lock) {
+			//assertion
+			Objects.requireNonNull(cbuf, "cbuf");
+			if (off < 0 || len < 0 || off + len > cbuf.length)
+				throw new IndexOutOfBoundsException();
+			if (len == 0)
+				return 0;
+			this.ensureOpen();
+
+			//REWIND
+			int rewind = 0;
+			if (this.buffer != null && this.buffer.hasNext()) {
+				rewind = this.buffer.read(cbuf, off, len);
+
+				//rewind is enough
+				if (rewind == len)
+					return rewind;
+
+				len = len - rewind;
+				off = len + rewind;
+			}
+
+			//NEW DATA
+			int read = this.reader.read(cbuf, off, len);
+
+			//the end
+			if (read == -1)
+				return rewind;
+
+			//sneak copy
+			if (this.buffer != null)
+				this.buffer.write(cbuf, off, read);
+
+			return rewind + read;
+		}
+	}
+
+	@Override
+	public long skip(long ns) throws IOException {
+		if (ns < 0)
+			throw new IllegalArgumentException("negative long isn't allowed here!");
+
+		int skip = ns > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) ns;
+
+		synchronized (super.lock) {
+			//assertion
+			this.ensureOpen();
+
+			//REWIND
+			int r0 = 0;
+			if (this.buffer != null)
+				r0 = this.buffer.skip(skip);
+
+			skip -= r0;
+			if (skip == 0) {
+				//REWIND is enough
+				return r0;
+			} else {
+				//to carry to the buffer
+				char[] tempo = new char[skip];
+
+				//read new data
+				int r1 = this.reader.read(tempo, 0, skip);
+
+				//No data have been returned!
+				if (r1 == 0 || r1 == -1) {
+					//only the rewind
+					return r0;
+				} else if (this.buffer != null) {
+					//sneak store
+					this.buffer.write(tempo, 0, r1);
+				}
+
+				return r0 + r1;
+			}
+		}
+	}
+
+	@Override
+	public boolean ready() throws IOException {
+		synchronized (super.lock) {
+			this.ensureOpen();
+			return this.reader.ready();
+		}
+	}
+
+	@Override
+	public boolean markSupported() {
+		return true;
+	}
+
+	@Override
+	public void mark(int readAheadLimit) {
+		this.mark(readAheadLimit, 50);
+	}
+
+	@Override
+	public void reset() throws IOException {
+		synchronized (super.lock) {
+			this.ensureOpen();
+			if (this.buffer == null)
+				throw new IOException("No mark to reset to!");
+
+			this.buffer.setCursor(0);
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		synchronized (super.lock) {
+			this.reader.close();
+			this.reader = null;
+			super.lock = null;
+			this.buffer = null;
+		}
+	}
+
+	/**
+	 * Make sure that this buffer is opened. Throw an IOException if it's not.
+	 *
+	 * @throws IOException if this reader is closed
+	 */
+	public void ensureOpen() throws IOException {
+		synchronized (super.lock) {
+			if (this.reader == null)
+				throw new IOException("Stream closed");
+		}
+	}
+
+	/**
+	 * Get the {@link Buffer} used CURRENTLY by this reader.
+	 *
+	 * @return the buffer used currently by this reader
+	 */
+	public Buffer<Integer> getBuffer() {
+		synchronized (super.lock) {
+			return this.buffer;
+		}
+	}
+
+	/**
+	 * Return whether this buffer is marked or not. Useful before invoking {@link #mark}.
+	 *
+	 * @return whether this is currently marked or not
+	 */
+	public boolean isMarked() {
+		synchronized (super.lock) {
+			return this.buffer != null;
+		}
+	}
+
+	/**
+	 * Marks the present position in the stream. Subsequent calls to reset() will attempt to reposition the stream to this point.
+	 *
+	 * @param readAheadLimit the initial capacity of the buffer
+	 * @param chunkSize      the size added on each buffer expand request
+	 * @throws IllegalArgumentException if the given 'readAheadLimit' is less than zero. Or if the given 'chunkSize' is not greater than zero
+	 */
+	public void mark(int readAheadLimit, int chunkSize) {
+		synchronized (super.lock) {
+			if (readAheadLimit < 0)
+				throw new IllegalArgumentException("readAheadLimit < 0");
+			if (chunkSize <= 0)
+				throw new IllegalArgumentException("chunkSize <= 0");
+
+			this.buffer = this.buffer == null ? new Buffer<>(readAheadLimit, chunkSize) : this.buffer.duplicate(this.buffer.getCursor(), chunkSize);
+		}
+	}
+
+	/**
+	 * Remove the last mark. (Remove the buffer)
+	 */
+	public void unmark() {
+		synchronized (super.lock) {
+			this.buffer = null;
+		}
+	}
+}
---
+++ src/main/java/cufy/nio/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019, LSafer, All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * -You can edit this file (except the header).
+ * -If you have change anything in this file. You
+ *   shall mention that this file has been edited.
+ *   By adding a new header (at the bottom of this header)
+ *   with the word "Editor" on top of it.
+ */
+/**
+ * All about buffers.
+ *
+ * @author LSaferSE
+ * @version 1 release (14-Feb-2020)
+ * @since 14-Feb-2020
+ */
+package cufy.nio;
