e679a3f9e640a6facb816cce0831790e6f38c70d
LSafer <lsaferse@gmail.com>
2020-04-02 16:31:44 +0300

    Moved all loadabes to 'cufyorg:misc'
    Not fitting here

--- src/main/java/org/cufy/net/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides the classes for implementing networking applications.
- *
- * @author LSaferSE
- * @version 1 release (16-Feb-20)
- * @since 16-Feb-20
- */
-package org.cufy.net;
---
+++ src/test/java/cufy/nio/BufferTest.java
@@ -0,0 +1,11 @@
+package cufy.nio;
+
+import org.junit.Test;
+
+@SuppressWarnings("JavaDoc")
+public class BufferTest {
+	@Test
+	public void test() {
+		//TODO
+	}
+}
